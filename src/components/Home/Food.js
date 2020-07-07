7bcc3c8856cb4cfc3e60d22661f2e9fcfd1e1317
Upinder Bains <bains@Upinders-MacBook-Pro.local>
2020-07-06 22:00:11 -0400

    fixed path title and footer

--- src/components/Home/Food.js
+++ src/components/Home/Food.js
@@ -1,7 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
-import Title from "../Title"
+import Title from '../title'
 import { Section } from "../../styles"
 import Img from "gatsby-image"
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Footer from "./Footer"
+import Footer from './footer'
 import GlobalStyles from "../styles/global"
 import Nav from "./Nav/nav"
 
